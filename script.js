4f2aa927cc411b9be2c2b68ef496d1807c159ce4
ed <edrick613@gmail.com>
2020-04-19 22:55:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+var ctx, canv;
+
+window.onload = function(){
+    canv = document.getElementById("gc");
+    console.log(canv);
+    ctx = canv.getContext("2d");
+    //ctx.fillStyle= "black";
+    document.addEventListener("keydown", keyPush);
+    setInterval(game, 1000/15);
+    
+}
+
+xv = yv = 0;//player velocity and direction
+gs= tc = 20;//grid size and tile count
+ax = ay = 15 //apple x and apple y
+trail = [];
+tail = 5;
+
+px = py = 10; //player postion
+function game(){
+    px += xv;
+    py += yv;
+    if (px < 0){
+        px = tc - 1;
+    }
+    if (px > tc-1){
+        px = 0;
+    }
+    if (py < 0){
+        py = tc - 1;
+    }
+    if (py > tc-1){
+        py = 0;
+    }
+    ctx.fillStyle= "black";
+    ctx.fillRect(0, 0,canv.width, canv.height);
+
+    ctx.fillStyle="lime";
+    for(var i=0; i < trail.length; i++){
+        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
+        if(trail[i].x==px && trail[i].y==py){
+            tail = 5;
+        }
+    }
+
+    trail.push({x:px, y:py});//object instiation of object literal
+    while (trail.length>tail){
+        trail.shift();
+    }
+    if(ax==px && ay==py){
+        tail++;
+        ax = Math.floor(Math.random()*tc);
+        ay = Math.floor(Math.random()*tc);
+    }
+    ctx.fillStyle= "red";
+    ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
+}
+var key = 37;
+function keyPush(evt) {
+    console.log(evt.keyCode);
+    key = evt.keyCode;
+    
+    switch(key){
+        case 37:
+            //left
+            xv = -1; yv = 0;
+        
+            break;
+        case 38:
+            //up
+            xv = 0; yv = -1;
+            break;
+        case 39:
+            //right
+            xv = 1; yv = 0;
+            break;
+        case 40:
+            //down
+            xv = 0; yv = 1;
+            break;
+    }
+}
